ff3bc023cc827dbb2872232284bb2bebce066b16
Matthew Godin <m2godin@edu.uwaterloo.ca>
2019-07-14 21:34:21 -0400

    View uses SDL

--- include/View.h
+++ include/View.h
@@ -1,13 +1,37 @@
 #ifndef VIEW_H_
 #define VIEW_H_
-#include "Observer.h"
-#include "Block.h"
-
+#include <SDL2/SDL.h>
+#include <string>
+#include <fstream>
+#include "../include/Observer.h"
+#include "../include/Block.h"
+#include "../include/View.h"
+class Subject;
 class View : public Observer {
     std::ostream &outStream;
+    std::ofstream errorFile;
+    const std::string ERR_FILE_LOC = "../logs/errors.log";
+    bool init(SDL_Window **window, SDL_Surface **screenSurface);
+    bool loadMedia(SDL_Surface **surface, char name[]);
+    void freeSurface(SDL_Surface **surface);
+    void close(SDL_Window **window);
+    const int SCREEN_WIDTH = 500;
+    const int SCREEN_HEIGHT = 800;
+    SDL_Surface *screenSurface;
+    SDL_Surface *I_Block;
+    SDL_Surface *J_Block;
+    SDL_Surface *L_Block;
+    SDL_Surface *O_Block;
+    SDL_Surface *S_Block;
+    SDL_Surface *Z_Block;
+    SDL_Surface *T_Block;
+    SDL_Rect position;
   public:
+    SDL_Window* window;
+    bool quit;
+    SDL_Event e;
     View(std::ostream &);
-    void notify(*Subject) override;
+    void notify(Subject*) override;
     ~View();
 };
 
--- src/View.cc
+++ src/View.cc
@@ -1,14 +1,116 @@
-#include <string>
-#include "../include/Observer.h"
-#include "../include/Block.h"
 #include "../include/View.h"
 
-View::View(std::ostream &sout) : outStream{sout} {}
-
-StreamView::~StreamView() {
-  subject->detach(this);
+View::View(std::ostream &sout) : outStream{sout} {
+    
+    window = NULL;
+    screenSurface = NULL;
+    I_Block = NULL;
+    J_Block = NULL;
+    L_Block = NULL;
+    O_Block = NULL;
+    S_Block = NULL;
+    Z_Block = NULL;
+    T_Block = NULL;
+    quit = false;
+    if (!init(&window, &screenSurface)) {
+        errorFile.open(ERR_FILE_LOC);
+        errorFile << "Failed to initialize!" << std::endl;
+        errorFile.close();
+    }
+    else {
+        if (!loadMedia(&I_Block, "../img/I.bmp") || !loadMedia(&J_Block, "../img/J.bmp") || !loadMedia(&L_Block, "../img/L.bmp") || !loadMedia(&O_Block, "../img/O.bmp") || !loadMedia(&S_Block, "../img/S.bmp") || !loadMedia(&Z_Block, "../img/Z.bmp") || !loadMedia(&T_Block, "../img/T.bmp")) {
+            errorFile.open(ERR_FILE_LOC);
+            errorFile << "Failed to load media!" << std::endl;
+            errorFile.close();
+        }
+        else {
+            position.x = 25;
+            position.y = 25;
+            SDL_BlitSurface(I_Block, NULL, screenSurface, &position);
+            position.x = 150;
+            position.y = 25;
+            SDL_BlitSurface(J_Block, NULL, screenSurface, &position);
+            position.x = 25;
+            position.y = 150;
+            SDL_BlitSurface(L_Block, NULL, screenSurface, &position);
+            position.x = 150;
+            position.y = 150;
+            SDL_BlitSurface(O_Block, NULL, screenSurface, &position);
+            position.x = 275;
+            position.y = 25;
+            SDL_BlitSurface(S_Block, NULL, screenSurface, &position);
+            position.x = 275;
+            position.y = 150;
+            SDL_BlitSurface(Z_Block, NULL, screenSurface, &position);
+            position.x = 25;
+            position.y = 275;
+            SDL_BlitSurface(T_Block, NULL, screenSurface, &position);
+            SDL_UpdateWindowSurface(window);
+        }
+    }
 }
 
-void StreamView::notify(Subject *subject) {
+View::~View() {
+    freeSurface(&I_Block);
+    freeSurface(&J_Block);
+    freeSurface(&L_Block);
+    freeSurface(&O_Block);
+    freeSurface(&S_Block);
+    freeSurface(&Z_Block);
+    freeSurface(&T_Block);
+    close(&window);
+    //subject->detach(this);
+}
+
+void View::freeSurface(SDL_Surface **surface) {
+	SDL_FreeSurface(*surface);
+	*surface = NULL;
+}
+
+void View::close(SDL_Window **window_) {
+	SDL_DestroyWindow(*window_);
+	*window_ = NULL;
+	SDL_Quit();
+}
+
+bool View::init(SDL_Window **window_, SDL_Surface **screenSurface_) {
+	bool success = true;
+	if(SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		errorFile.open(ERR_FILE_LOC);
+		errorFile << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+		errorFile.close();
+	}
+	else
+	{
+		*window_ = SDL_CreateWindow("Quadris", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+		if(*window_ == NULL)
+		{
+                        errorFile.open(ERR_FILE_LOC);
+			errorFile << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+                        errorFile.close();
+			success = false;		
+		}
+		else
+		{
+			*screenSurface_ = SDL_GetWindowSurface(*window_);
+		}
+	}
+	return success;
+}
+
+bool View::loadMedia(SDL_Surface **surface, char name[]) {
+	bool success = true;
+	*surface = SDL_LoadBMP(name);
+	if (*surface == NULL) {
+            errorFile.open(ERR_FILE_LOC);
+            errorFile << "Unable to load image " << name << "! SDL Error: " << SDL_GetError() << std::endl;
+            errorFile.close();
+	    success = false;	
+	}
+	return success;
+}
+
+void View::notify(Subject *subject) {
   
 }
